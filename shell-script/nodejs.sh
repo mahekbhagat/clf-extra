1d5c11588b35f66b0a60335753ccc63c032d75ed
mahekcrest <mahek.b@crestinfosystems.net>
2022-11-24 17:18:42 +0530

    shell script for node js

---
+++ shell-script/nodejs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+if [ $# -ne 1 ];then
+  echo "Pass single parameter"
+  exit 1
+elif
+  nodever=$(echo "node-14-xx-lts" | grep -e "node-[1-9][0-9]-xx-lts" | grep -v grep | awk -F'-' '{print $2}')
+  echo "Installing node $nodever.x"
+else
+  echo "Invalid Parmeter"
+  exit 1
+fi
+
+echo "node version"
+cd /tmp/
+curl -sL https://deb.nodesource.com/setup_$nodever.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
