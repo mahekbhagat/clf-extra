c4e09215016a5b7e4f15147b2e2d9f2f80c32aa2
mahekcrest <mahek.b@crestinfosystems.net>
2022-11-28 13:57:08 +0530

    Changes in common.sh

--- shell-script/common.sh
+++ shell-script/common.sh
@@ -5,6 +5,13 @@ if [ $# -ne 1 ] || [ -z $1 ];then
   exit 1
 fi
 
+## Set non-interactive session and auto restart dependent services
+export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
+
+## Set hostname
+#hostnamectl set-hostname 
+
 nodever=$(echo "$1" | awk -F ',' '{  for (i=1; i<=NF; i++) print $i }' | grep "node" | grep -v grep | awk -F'=' '{print $2}' | awk -F'-' '{print $2}')
 phpver=$(echo "$1" | awk -F ',' '{  for (i=1; i<=NF; i++) print $i }' | grep "php" | grep -v grep |awk -F'=' '{print $2}' | sed 's/php//g' | sed 's/-/./g')
 webser=$(echo "$1" | awk -F ',' '{  for (i=1; i<=NF; i++) print $i }' | grep "webserver" | grep -v grep | awk -F'=' '{print $2}' | awk '{print tolower($0)}')
@@ -24,9 +31,6 @@ fi
 ##
 if [ "$phpver" != "No" ] && [ ! -z $nodever ];then
 
-  export NEEDRESTART_MODE=a
-  export DEBIAN_FRONTEND=noninteractive
-
   sudo add-apt-repository --yes ppa:ondrej/php
   sudo apt install -y php$phpver
   echo "Installing PHP-$phpver modules"
@@ -34,6 +38,8 @@ if [ "$phpver" != "No" ] && [ ! -z $nodever ];then
   phpmodules="common,fpm,json,cli,zip,bz2,mysql,xmlrpc,dev,imap,gd,xml,opcache,mcrypt,bcmath,curl,intl,mbstring,soap,xsl,imagick,cgi,mongodb"
   echo "Installing PHP-$phpver modules"
   echo "$phpmodules" | awk -F ',' '{ for (i=1; i<=NF; i++) system("sudo apt install -y php'$phpver'-" $i ) }'
+
+  # Restart any pending service 
   sudo needrestart -u NeedRestart::UI::stdio -r a
 fi
 
