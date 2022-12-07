edc526aa909102e99766831b1006c89daca32c40
mahekcrest <mahek.b@crestinfosystems.net>
2022-12-07 14:44:14 +0530

    mongodb

--- shell-script/mongodb.sh
+++ shell-script/mongodb.sh
@@ -5,11 +5,17 @@ if [ $# -ne 1 ] || [ -z $1 ];then
   exit 1
 fi
 
+if [ "$DB" != "MongoDB" ];then
+  echo "DB type not mongodb"
+  exit 0
+fi
+
 echo "Mongodb latest version"
 
 os_code=`cat /etc/os-release | grep -i "UBUNTU_CODENAME" | awk -F'=' '{print $2}'`
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${os_code}/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6A26B1AE64C3C388
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
