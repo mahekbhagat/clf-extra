feae2eaea39c54223f1707ed9bbfdb0cb0222140
mahekcrest <mahek.b@crestinfosystems.net>
2022-12-27 18:00:50 +0530

    merge

--- shell-script/DBinstall.sh
+++ shell-script/DBinstall.sh
@@ -9,15 +9,12 @@ fi
 ## DB Type
 DatabaseType=$(echo "$1" | sed 's/,/\n/g' | grep -i DatabaseType | awk -F'=' '{print $2}')
 
-if [ "$DatabaseType" != "MongoDB" ];then
-  echo "DB type not mongodb"
+if [ "$DatabaseType" != "MongoDB" ] && [ "$DatabaseType" != "MYSQL" ];then
+  echo "Selected DB type not mongodb or mysql"
   exit 0
 fi
 
-if [ "$(echo $1 | sed 's/,/\n/g' | wc -l)" -ne 5 ];then
-  echo "Please pass all 5 value in argument sperated by comma"
-  exit 0
-fi
+## Fetch DB details
 # echo "DatabaseType=Mongodb,DBName=test1,DBUser1=mahek,DBUser1Password=mahek123" | sed 's/,/\n/g' | grep -i DatabaseType | awk -F'=' '{print $2}'
 
 ## User1
@@ -31,62 +28,82 @@ DBAdminUser=$(echo "$1" | sed 's/,/\n/g' | grep -i DBAdminUser | awk -F'=' '{pri
 ## Admin user Password
 DBAdminPassword=$(echo "$1" | sed 's/,/\n/g' | grep -i DBAdminPassword | awk -F'=' '{print $2}')
 
-## Ubuntu code name
-os_code=`cat /etc/os-release | grep -i "UBUNTU_CODENAME" | awk -F'=' '{print $2}'`
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${os_code}/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6A26B1AE64C3C388
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-database hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-mongosh hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-
-## Allocate 1/4 of ram memory to mongo
-mongoconf="/etc/mongod.conf"
-TotalMemory=`free --giga | grep -i "Mem" | awk '{print $2}'`
-divideby=4
-memallocate=$(printf "%0.2f\n" $(echo "scale=2; $TotalMemory / $divideby" | bc))
-echo "$memallocate"
-
-if [ $(grep -i '^  journal:' $mongoconf | wc -l) -eq 1 ];then
-	echo "Skip adding journal"
-else
-	sed -i 's/storage:/storage:\n  journal:\n    enabled: true/g' $mongoconf
-fi
-
-if [ $(grep -i "^  wiredTiger:" $mongoconf | wc -l) -ne 1 ];then
-	sed -i 's/#  wiredTiger:/  wiredTiger:\n    engineConfig:\n      cacheSizeGB: '$memallocate'/g' $mongoconf
-else
-	echo "Found wiredTiger in config. Memory already allocated"
-fi
-
-## start and enable mongod service
-systemctl start mongod 
-systemctl enable mongod
-
+DBEndpoint=$(echo "$1" | sed 's/,/\n/g' | grep -i DBEndpoint | awk -F'=' '{print $2}')
 ##
-## create user
+if [ "$DatabaseType" == "MongoDB" ];then
+  if [ "$(echo $1 | sed 's/,/\n/g' | wc -l)" -ne 5 ];then
+    echo "Please pass all 5 value in argument sperated by comma"
+    exit 0
+  fi
 
-mongosh --quiet --eval << EOF
-  use ${DBName}
-  db.createUser({ user: "${DBUser1}", pwd: "${DBUser1Password}", roles: [ { role: "readWrite", db: "${DBName}" } ]})
-EOF
+  ## Ubuntu code name
+  os_code=`cat /etc/os-release | grep -i "UBUNTU_CODENAME" | awk -F'=' '{print $2}'`
 
-## Create admin user
-mongosh --quiet --eval << EOF
-  use admin
-  db.createUser({ user: "admin" , pwd: "${DBAdminPassword}", roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]})
-EOF
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${os_code}/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6A26B1AE64C3C388
+  sudo apt-get update
+  sudo apt-get install -y mongodb-org
 
-## Enable security
-if [ $(grep -i "^security:" $mongoconf | wc -l) -ne 1 ];then
-  echo "security:" >> $mongoconf
-  echo "  authorization: \"enabled\"" >> $mongoconf
+  echo "mongodb-org hold" | sudo dpkg --set-selections
+  echo "mongodb-org-database hold" | sudo dpkg --set-selections
+  echo "mongodb-org-server hold" | sudo dpkg --set-selections
+  echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+  echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+  echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+  ## Allocate 1/4 of ram memory to mongo
+  mongoconf="/etc/mongod.conf"
+  TotalMemory=`free --giga | grep -i "Mem" | awk '{print $2}'`
+  divideby=4
+  memallocate=$(printf "%0.2f\n" $(echo "scale=2; $TotalMemory / $divideby" | bc))
+  echo "$memallocate"
+
+  if [ $(grep -i '^  journal:' $mongoconf | wc -l) -eq 1 ];then
+    echo "Skip adding journal"
+  else
+    sed -i 's/storage:/storage:\n  journal:\n    enabled: true/g' $mongoconf
+  fi
+
+  if [ $(grep -i "^  wiredTiger:" $mongoconf | wc -l) -ne 1 ];then
+    sed -i 's/#  wiredTiger:/  wiredTiger:\n    engineConfig:\n      cacheSizeGB: '$memallocate'/g' $mongoconf
+  else
+    echo "Found wiredTiger in config. Memory already allocated"
+  fi
+
+  ## start and enable mongod service
+  systemctl start mongod 
+  systemctl enable mongod
+
+  ##
+  ## create user
+
+  mongosh --quiet --eval << EOF
+    use ${DBName}
+    db.createUser({ user: "${DBUser1}", pwd: "${DBUser1Password}", roles: [ { role: "readWrite", db: "${DBName}" } ]})
+  EOF
+
+  ## Create admin user
+  mongosh --quiet --eval << EOF
+    use admin
+    db.createUser({ user: "admin" , pwd: "${DBAdminPassword}", roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]})
+  EOF
+
+  ## Enable security
+  if [ $(grep -i "^security:" $mongoconf | wc -l) -ne 1 ];then
+    echo "security:" >> $mongoconf
+    echo "  authorization: \"enabled\"" >> $mongoconf
+  fi
+
+  # mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+elsif [ "$DatabaseType" == "MYSQL" ];then
+
+  mysql -h"${DBEndpoint}" -u${DBAdminUser} -p${DBAdminPassword} << EOF
+    CREATE USER "${DBUser1}"@'%' IDENTIFIED WITH mysql_native_password BY "'${DBUser1Password}'";
+    GRANT ALL PRIVILEGES ON `%`.* To "'${DBUser1}'"@'%';
+    FLUSH PRIVILEGES;
+  EOF
+
+else
+  echo "Wrong database type value passed"
+  exit 0
 fi
-
-# mongo --eval 'db.runCommand({ connectionStatus: 1 })'
